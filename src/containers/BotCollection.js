83d78ded1258a1dd11fda04003f3acf57437e2b0
YSongYS <songyishu.yang@gmail.com>
2019-03-20 10:01:14 +0000

    at check point

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -8,8 +8,7 @@ class BotCollection extends React.Component {
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.props.allBots.map(bot=> !!bot && <BotCard bot={bot} recruitBot={this.props.recruitBot}/>)}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,37 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state = {
+    allBots:[],
+    myBots:[]
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(res=>res.json())
+      .then(data=>this.setState({
+        allBots:[...data]
+      }))
+  }
+
+  botNotMine = () => {
+    return (this.state.allBots.filter(bot=>!this.state.myBots.includes(bot)))
+  }
+
+  recruitBot = (botHired) => {
+    this.setState({
+      myBots:this.state.myBots.concat(botHired)
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy myBots={this.state.myBots} />
+        <BotCollection allBots={this.botNotMine()} recruitBot={this.recruitBot} />
       </div>
     );
   }
